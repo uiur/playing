e5b6b031af9ef126a4f0c284f691871d465eb8b9
Kazato Sugimoto <uiureo@gmail.com>
2015-08-31 22:36:07 +0900

    Show success message

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const React = require('react')
 const ipc = require('ipc')
+const shell = require('shell')
+const url = require('url')
 
 const App = React.createClass({
   getInitialState () {
@@ -18,31 +20,50 @@ const App = React.createClass({
     ipc.send('data', this.state)
   },
 
+  inputIsValid () {
+    return url.parse(this.state.slack).host === 'hooks.slack.com'
+  },
+
   render () {
     const self = this
 
     return (
-      React.DOM.div({}, [
-        React.DOM.button({
-          className: 'btn btn-default',
-          onClick: function () {
-            ipc.send('terminate')
-          }
-        }, 'Quit'),
-
+      React.DOM.main({
+        className: 'container'
+      }, [
         React.DOM.div({}, [
-          React.DOM.h2({}, 'Slack'),
+          React.DOM.h2({}, 'playing'),
+          React.DOM.p({ className: 'description' }, [
+            React.DOM.a({
+              href: 'https://slack.com/services/new/incoming-webhook',
+              onClick: function (e) {
+                e.preventDefault()
+                shell.openExternal(e.target.href)
+              }
+            }, 'Add a new Incoming Webhook in Slack'),
+            '\nand paste the webhook URL to this form.'
+          ]),
+
+          React.DOM.label({}, 'Slack Webhook URL'),
           React.DOM.input({
             className: 'form-control',
             type: 'text',
-            placeholder: 'slack webhook url',
+            placeholder: 'https://hooks.slack.com/...',
             value: this.state.slack,
             onChange: function (e) {
               self.setState({ slack: e.target.value })
               window.localStorage.setItem('slack', e.target.value)
             }
-          })
-        ])
+          }),
+          this.inputIsValid() && React.DOM.div({ className: 'alert alert-success' }, '✔ Play music on iTunes!')
+        ]),
+
+        React.DOM.button({
+          className: 'btn btn-default btn-quit',
+          onClick: function () {
+            ipc.send('terminate')
+          }
+        }, 'Quit')
       ])
     )
   }
